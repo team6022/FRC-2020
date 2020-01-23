72561c0408ab577ba428859db42bb711f2f1b5f1
Matt Steiner <msteinerweb@gmail.com>
2020-01-23 17:47:43 -0600

    work on driving

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -23,7 +23,36 @@ public class RobotMap {
   public static final int Wheel = 4;
 
   // CAN Devices
-  public static final int ShootMotor = 0;
+  public static int leftMasterPort = 3;
+  public static int leftSlavePort = 2;
+  public static int rightMasterPort = 0;
+  public static int rightSlavePort = 1;
+
+  public static final int ShootMotor = 8;
+
+  // TALON Settings
+  public static final int slot0 = 0;
+  public static final int slot1 = 1;
+  public static final int slot2 = 2;
+  public static final int slot3 = 3;
+
+  public static final int pidPrimary = 0;
+  public static final int pidTurn = 1;
+
+  public static final int timeoutMs = 30;
+  public static final int baseTrajPeriodMs = 0;
+  
+  public static final double driveNeutralDeadband = 0.1;
+
+  // 30AMP CURRENT LIMITS
+  public static final int current30AmpPeakCurrentLimit = 25;
+  public static final int current30AmpPeakCurrentDuration = 200;
+  public static final int current30AmpContinuousCurrentLimit = 25;
+
+  // 40AMP CURRENT LIMITS
+  public static final int current40AmpPeakCurrentLimit = 35;
+  public static final int current40AmpPeakCurrentDuration = 200;
+  public static final int current40AmpContinuousCurrentLimit = 35;
 
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
